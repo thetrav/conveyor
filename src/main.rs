04ab94c71e360ff583f454309ecafe90aed79e70
Travis DIxon <the.trav@gmail.com>
2023-07-29 10:52:12 +1000

    tool follow mouse

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
 
 #[derive(Component)]
 struct Person;
@@ -15,8 +16,19 @@ struct AnimationIndices {
     last: usize,
 }
 
+#[derive(Component)]
+struct CursorTool {
+    first: usize,
+    last: usize,
+}
+
 #[derive(Component, Deref, DerefMut)]
 struct AnimationTimer(Timer);
+
+#[derive(Component)]
+struct MainCamera;
+
+
 
 fn main() {
     App::new()
@@ -44,19 +56,18 @@ fn setup(mut commands: Commands,
 
     let animation_indices = AnimationIndices { first: 24, last: 26 };
 
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn((Camera2dBundle::default(), MainCamera));
 
-    // commands.spawn(SpriteSheetBundle {
-    //     texture_atlas: factory_atlas_handle,
-    //     sprite: TextureAtlasSprite::new(0),
-    //     transform: Transform::from_scale(Vec3::splat(6.0)),
-    //     ..default()
-    // });
+    commands.spawn((SpriteSheetBundle {
+        texture_atlas: factory_atlas_handle,
+        sprite: TextureAtlasSprite::new(85),
+        transform: Transform::from_xyz(50.0, 5.0, 0.0),
+        ..default()
+    }, CursorTool {first: 85, last: 87}));
 
     commands.spawn((SpriteSheetBundle {
         texture_atlas: character_atlas_handle,
         sprite: TextureAtlasSprite::new(animation_indices.first),
-        transform: Transform::from_scale(Vec3::splat(6.0)),
         ..default()
         },
        animation_indices,
@@ -85,11 +96,32 @@ fn animate_sprite(
     }
 }
 
+fn follow_mouse(
+    windows_q: Query<&Window, With<PrimaryWindow>>,
+    camera_q: Query<(&Camera, &GlobalTransform), With<MainCamera>>,
+    mut cursor_q: Query<(
+        &CursorTool,
+        &mut Transform,
+    )>,
+) {
+    let (camera, camera_transform) = camera_q.single();
+    if let Some(position) = windows_q.single().cursor_position()
+        .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
+        .map(|ray| ray.origin.truncate())
+    {
+        for (_, mut xform) in &mut cursor_q {
+            xform.translation.x = position.x;
+            xform.translation.y = position.y;
+        }
+    }
+}
+
 pub struct ConveyorPlugin;
 
 impl Plugin for ConveyorPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup)
-            .add_systems(Update, animate_sprite);
+            .add_systems(Update, animate_sprite)
+            .add_systems(Update, follow_mouse);
     }
 }
