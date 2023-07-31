8a573730f58d8a3a0ad6b20c8172c8c57a05d6aa
Travis DIxon <the.trav@gmail.com>
2023-07-31 19:41:18 +1000

    fix asset path

--- conveyor.js
+++ conveyor.js
@@ -1895,7 +1895,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('https://github.com/thetrav/conveyor/releases/download/0.1.0/conveyor_bg.wasm', import.meta.url);
+        input = new URL('https://github.com/thetrav/conveyor/releases/download/0.1.0/conveyor.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
